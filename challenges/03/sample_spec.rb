ad18ae63cb5765f7828c77c1491a23b6b5f8a17b
bikezilla <v.konushliev@gmail.com>
2013-11-04 22:55:42 +0200

    add specs for challenge 03

--- challenges/03/sample_spec.rb
+++ challenges/03/sample_spec.rb
@@ -10,4 +10,21 @@ describe "Object.thread" do
   it "works with multiple operations" do
     "42".thread(:to_i, ->(x) { x / 6 }, :succ, ->(x) { x ** 2 }).should eq 64
   end
+
+  it 'works with -42' do
+    -42.thread(:abs, -> x { x ** 3 }, :succ, -> x { x - 2 }).should eq 74087
+  end
+
+  it 'works with Marvin' do
+    "Marvin".thread(:size, -> x { x ** 4 }, :to_s, -> s { s.split "" }, -> a { a[0] + a[-1] }, :to_i).should eq 16
+  end
+
+  it 'works with no args' do
+    42.thread().should eq 42
+  end
+
+  it 'works with Procs' do
+    42.thread(Proc.new{ |x| x / 2 }, Proc.new{ |x| x - 1 }).should eq 20
+  end
+
 end
