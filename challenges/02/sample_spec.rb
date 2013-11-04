3b7116ae06002ae50b235851fcd56a19b6ea4ba6
bikezilla <v.konushliev@gmail.com>
2013-11-04 22:54:39 +0200

    add more specs to challenge 2

--- challenges/02/sample_spec.rb
+++ challenges/02/sample_spec.rb
@@ -3,4 +3,20 @@ describe "homogenize" do
     homogenize([1, :a, 2, :b, 3, :c]).should    =~ [[1, 2, 3], [:a, :b, :c]]
     homogenize([:a, "Jimi", "Kurt", :b]).should =~ [[:a, :b], ["Jimi", "Kurt"]]
   end
+
+  it "returns empty array when given an empty array" do
+    homogenize([]).should =~ []
+  end
+
+  it "returns array of homogeneous arrays" do
+    homogenize([1, :a, 2, :b, 3, :c]).should =~ [[1, 2, 3], [:a, :b, :c]]
+    homogenize([:a, "Jimi", "Kurt", :b]).should =~ [[:a, :b], ["Jimi", "Kurt"]]
+    homogenize([:a, "Kurt", :b]).should =~ [["Kurt"], [:a, :b]]
+    homogenize([:a,32,:b,3,"gosho"]).should =~ [[:a, :b], [32,3],["gosho"]]
+    homogenize([[],{},Hash.new(0),3,'gosho']).should =~ [[{},{}], [[]],[3],['gosho']]
+    homogenize([1.3,1/3r,:b,3,Array.new]).should =~ [[:b], [1/3r], [1.3],[3],[[]]]
+    homogenize([]).should =~ []
+    homogenize([nil,[nil]]).should =~ [[nil],[[nil]]]
+    homogenize([NilClass,Array]).should =~ [[NilClass,Array]]
+  end
 end
