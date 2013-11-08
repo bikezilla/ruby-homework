652571e0adccd1b04ba1c5066892f5e06ea9f3ab
bikezilla <v.konushliev@gmail.com>
2013-11-08 09:51:18 +0200

    add more spec for challenge 6

--- challenges/06/sample_spec.rb
+++ challenges/06/sample_spec.rb
@@ -6,4 +6,12 @@ describe Polynomial do
   it "formats a polynomial which contains negative and zero coefficients" do
     Polynomial.new([-3, -4, 1, 0, 6]).to_s.should eq '- 3x^4 - 4x^3 + x^2 + 6'
   end
+
+  it "returns 0 for polynomial with only zero coefficients" do
+    Polynomial.new([0, 0, 0]).to_s.should eq '0'
+  end
+
+  it 'returns empty string for polynomial with empty coefficients' do
+    Polynomial.new([]).to_s.should eq ''
+  end
 end
